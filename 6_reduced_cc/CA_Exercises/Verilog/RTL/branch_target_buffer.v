3a6ad226286e44196fe47a82eb5941e3ceab6f48
mmonden <matt.mll@hotmail.com>
2023-05-20 17:19:43 +0200

    BHT updated

--- 6_reduced_cc/CA_Exercises/Verilog/RTL/branch_target_buffer.v
+++ 6_reduced_cc/CA_Exercises/Verilog/RTL/branch_target_buffer.v
@@ -18,22 +18,22 @@ module branch_target_buffer#(
    reg [63:0] r_predicted_branch_pc;
 
 	reg [64-LOWER+63:0] states[0:2**LOWER - 1]; // higer_pc_part (x-bits) | branch_pc_part (64-bits)
-	initial for(i = 0; i < 2**LOWER; i++) states[i] <= 0;
+	initial for(i = 0; i < 2**LOWER; i++) states[i] = 0;
 
 	always@(posedge clk, negedge arst_n) begin
 		if(arst_n==0)begin
 			   r_predicted_branch_pc <= 0;
-            for(i = 0; i < 2**LOWER; i++) states[i] <= 0;
+            for(i = 0; i < 2**LOWER; i++) states[i] = 0;
 		end
 
       if(en == 1'b1) begin
 			row_index = current_pc[LOWER - 1:0];
 
          if(was_taken)
-			   states[row_index] <= {prev_pc[64-LOWER+63:64], branch_pc};
+			   states[row_index] = {prev_pc[64-LOWER+63:64], branch_pc};
 
          if(jumped)
-            states[row_index] <= {prev_pc[64-LOWER+63:64], jump_pc};
+            states[row_index] = {prev_pc[64-LOWER+63:64], jump_pc};
 
 			if(~|(current_pc[63:LOWER] ^ states[row_index][64-LOWER+63:64])) 
             r_predicted_branch_pc <= states[row_index][63:0];
